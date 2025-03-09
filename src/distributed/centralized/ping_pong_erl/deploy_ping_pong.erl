c347ce4511550ab18fa26a25306f827114cf8b68
Botond Szirtes <botond.szirtes@est.tech>
2025-03-09 20:38:16 +0100

    Update deploy_ping_pong.erl script

--- src/distributed/centralized/ping_pong_erl/deploy_ping_pong.erl
+++ src/distributed/centralized/ping_pong_erl/deploy_ping_pong.erl
@@ -2,35 +2,35 @@
 -export([deploy/1]).
 
 deploy(Args) ->
-    io:format("Args: ~p\n", [Args]),
     case parse_args(Args) of
-        {ok, NumClients, PingCount, DelayMs, NoServer, Verbose} ->
-            launch_deployment(NumClients, PingCount, DelayMs, NoServer, Verbose);
+        {ok, Nodes, NumClients, PingCount, DelayMs, NoServer, Verbose} ->
+            launch_deployment(Nodes, NumClients, PingCount, DelayMs, NoServer, Verbose);
         {error, Msg} ->
-            io:format("Error: ~s~nUsage: deploy <num_clients> <ping_count> <delay_ms> [no-server] [verbose]~n", [Msg])
+            io:format("Error: ~s~nUsage: deploy \"[node1@host, node2@host, ...]\" <num_clients> <ping_count> <delay_ms> [--no-server]~n", [Msg])
     end.
 
-parse_args([NumClientsStr, PingCountStr, DelayMsStr | Rest]) ->
+parse_args([NodesStr, NumClientsStr, PingCountStr, DelayMsStr | Rest]) ->
+    Nodes = parse_nodes(NodesStr),
     case {string:to_integer(NumClientsStr), string:to_integer(PingCountStr), string:to_integer(DelayMsStr)} of
         {{IntClients, _}, {IntPings, _}, {IntDelay, _}} ->
             NoServer = lists:member("no-server", Rest),
             Verbose = lists:member("verbose", Rest),
-            {ok, IntClients, IntPings, IntDelay, NoServer, Verbose};
+            {ok, Nodes, IntClients, IntPings, IntDelay, NoServer, Verbose};
         _ ->
             {error, "Invalid arguments."}
     end;
 parse_args(_) ->
     {error, "Invalid number of arguments."}.
 
-launch_deployment(NumClients, PingCount, DelayMs, NoServer, Verbose) ->
-    Nodes = ['server@localhost', 'client@localhost'],
-    Handler = self(),
-    start_server(hd(Nodes), NoServer, Verbose),
-    io:format("~p ~p ~p~n", [NumClients, NoServer, Verbose]),
-    spawn('client@localhost', fun() -> ping_pong_client:start('server@localhost', PingCount, DelayMs, Handler, Verbose) end),
-    receive
-        {Pid, done} -> io:format("Client ~p finished.~n", [Pid])
-    end,
+parse_nodes(NodesStr) ->
+    Stripped = string:trim(NodesStr, both, "[]"),
+    Parts = string:split(Stripped, ",", all),
+    [list_to_atom(string:trim(P)) || P <- Parts].
+
+launch_deployment(Nodes, NumClients, PingCount, DelayMs, NoServer, Verbose) ->
+    ServerNode = hd(Nodes),
+    start_server(ServerNode, NoServer, Verbose),
+    distribute_clients(Nodes, NumClients, PingCount, DelayMs, Verbose, ServerNode),
     io:format("All clients finished, terminating.~n"),
     stop_server(NoServer).
 
@@ -45,4 +45,23 @@ stop_server(true) ->
 stop_server(_) ->
     io:format("Stopping server...~n"),
     global:send(ping_pong_server, stop).
+
+distribute_clients(Nodes, NumClients, PingCount, DelayMs, Verbose, ServerNode) ->
+    Handler = self(),
+    NumNodes = length(Nodes),
+    io:format("Distributing ~p clients across ~p nodes (including server node).~n", [NumClients, NumNodes]),
+
+    lists:foreach(fun(Index) ->
+        Node = lists:nth((Index rem NumNodes) + 1, Nodes),  % Round-robin distribution
+        io:format("Starting client ~p on node ~s...~n", [Index, Node]),
+        spawn(Node, fun() -> ping_pong_client:start(ServerNode, PingCount, DelayMs, Handler, Verbose) end)
+    end, lists:seq(1, NumClients)),
+
+    io:format("All clients started.~n"),
+
+    lists:foreach(fun(_) ->
+        receive
+            {Pid, done} -> io:format("Client ~p finished.~n", [Pid])
+        end
+    end, lists:seq(1, NumClients)).
 
